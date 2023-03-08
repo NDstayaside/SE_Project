50b0afac74ce3d646758edffefcfe3546fb650e9
iluamm <tanawatlp@gmail.com>
2023-03-09 00:21:55 +0700

    change a little bit

---
+++ footer.php
@@ -0,0 +1,83 @@
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Noto+Sans+Thai&display=swap" rel="stylesheet">
+<style>
+footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background-color: #;
+    color: #ffffff;
+    font-size: 16px;
+    min-height:100px;
+    width:100%;
+    font-family: 'Noto Sans Thai', sans-serif;
+    padding-bottom: 2rem;
+}
+footer .container {
+  display: flex;
+  justify-content: space-between;
+  max-width: 60%;
+  margin: 0 auto;
+}
+
+footer .column {
+  flex-basis: 33%;
+}
+
+footer h3 {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+  color:#000000;
+}
+
+footer ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+footer li {
+  margin-bottom: 0.5rem;
+  color:#000000;
+}
+footer a{
+    text-decoration: none;
+    color:#000000;
+}
+footer a:hover{
+    color:#A0A0A0;
+}
+
+</style>
+</head>
+<footer>
+
+<hr width=100% size=2 color="#A0A0A0">
+<div class="container">
+    <div class="column">
+      <h3>ติดต่อเรา</h3>
+      <ul>
+        <li><a href="#">FB : Polaroid Shop</a></li>
+        <li><a href="#">Line :@Polaroid_shop</a></li>
+      </ul>
+    </div>
+    <div class="column">
+      <h3>&nbsp;</h3>
+      <ul>
+        <li><a href="#">Mail : polaroidshop@gmail.com</a></li>
+        <li><a href="#">Phone : 02-555-5555 / 085-555-5555</a></li>
+      </ul>
+    </div>
+    <div class="column">
+      <h3>&nbsp;</h3>
+      <ul>
+        <li>11/12 ซอย 5 ถนนข้าวสาร แขวงข้าวสาร ตำบลข้าวสาร จังหวัดข้าวสวย เลขไปรษณีย์ 55555 </li>
+      </ul>
+    </div>
+  </div>
+  
+</footer>
+</html> 
--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 <html>
 <body>
 
+<!-- <?php
+include("navbar.php")
+?> -->
+
 <h2>Hello World</h2>
 <textarea style="width:50%;height:30em;"></textarea>
+
 </body>
+<?php
+
+ include("footer.php")
+
+?>
 </html>
